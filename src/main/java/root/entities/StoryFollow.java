7c0e3d20fc0a1e9fba87e98da94e0b0608ee7582
ptbn271 <141623641+ptbn271@users.noreply.github.com>
2025-05-24 20:04:37 +0700

    add models

---
+++ src/main/java/root/entities/StoryFollow.java
@@ -0,0 +1,61 @@
+package root.entities;
+
+import java.sql.Date;
+
+public class StoryFollow {
+    private long id;
+    private Integer userId;
+    private Integer storyId;
+    private Date dayCreate;
+    private Integer follow;
+
+    public StoryFollow() {}
+
+    public StoryFollow(long id, Integer userId, Integer storyId, Date dayCreate, Integer follow) {
+        this.id = id;
+        this.userId = userId;
+        this.storyId = storyId;
+        this.dayCreate = dayCreate;
+        this.follow = follow;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getStoryId() {
+        return storyId;
+    }
+
+    public void setStoryId(Integer storyId) {
+        this.storyId = storyId;
+    }
+
+    public Date getDayCreate() {
+        return dayCreate;
+    }
+
+    public void setDayCreate(Date dayCreate) {
+        this.dayCreate = dayCreate;
+    }
+
+    public Integer getFollow() {
+        return follow;
+    }
+
+    public void setFollow(Integer follow) {
+        this.follow = follow;
+    }
+}
